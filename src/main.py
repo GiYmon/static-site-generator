0065140314d30de9b19c61d17cb0776f9121f5ab
GiYmon <szymongilo@gmail.com>
2025-08-03 20:41:01 +0200

    fix github pages

--- src/main.py
+++ src/main.py
@@ -12,10 +12,7 @@ template_path = "./template.html"
 
 
 def main():
-    if len(sys.argv) > 1:
-        basepath = sys.argv[1]
-    else:
-        basepath = "/"
+    basepath = sys.argv[1] if len(sys.argv) > 1 else "/"
 
     print("Deleting public directory...")
     if os.path.exists(dir_path_public):
